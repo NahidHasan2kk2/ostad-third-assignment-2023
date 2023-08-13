d5b980077da3d9f68bbf27e48d3863b582d25d28
NahidHasan2kk2 <nahidhasan2kk2@gmail.com>
2023-08-13 22:41:42 +0600

    another commit

--- main.js
+++ main.js
@@ -9,48 +9,138 @@ function printObjectProperties(obj) {
     }
 }
 
-// Example usage
-const myObject = {
+// Test cases
+const person = {
     name: "John",
     age: 30,
-    city: "New York"
-};
-2.function printObjectProperties(obj) {
-    if (Object.keys(obj).length === 0) {
-        console.log("Object is empty");
-    } else {
-        for (let prop in obj) {
-            console.log(`${prop}: ${obj[prop]}`);
-        }
-    }
-}
-
-// Example usage
-const myObject = {
-    name: "John",
-    age: 30,
-    city: "New York"
+    profession: "Engineer"
 };
 
-printObjectProperties(myObject);
+const emptyObj = {};
 
-3.function findMaxNumber(numbers) {
+printObjectProperties(person);
+printObjectProperties(emptyObj);
+
+2. function findMaxNumber(numbers) {
     if (numbers.length === 0) {
         return null;
     }
 
-    let maxNumber = numbers[0];
-
+    let max = numbers[0];
     for (let i = 1; i < numbers.length; i++) {
-        if (numbers[i] > maxNumber) {
-            maxNumber = numbers[i];
+        if (numbers[i] > max) {
+            max = numbers[i];
         }
     }
 
-    return maxNumber;
+    return max;
+}
+
+// Test cases
+const numbers1 = [10, 20, 5, 30, 25];
+console.log(findMaxNumber(numbers1));
+
+const numbers2 = [5, 15, 7];
+console.log(findMaxNumber(numbers2)); // Output: 15
+
+const emptyArray = [];
+console.log(findMaxNumber(emptyArray)); // Output: null
+
+
+3.function mergeArrays(arr1, arr2) {
+    return [...arr1, ...arr2];
+}
+
+// Test case
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+console.log(mergeArrays(arr1, arr2)); // Output: [1, 2, 3, 4, 5, 6]
+
+
+4.
+
+const calculateSquare = (number) => number * number;
+
+// Example usage
+console.log(calculateSquare(5)); // Output: 25
+console.log(calculateSquare(8)); // Output: 64
+
+
+5.
+
+const doubleNumbers = (numbers) => numbers.map(number => number * 2);
+
+// Example usage
+const numbers = [1, 2, 3, 4, 5];
+console.log(doubleNumbers(numbers)); // Output: [2, 4, 6, 8, 10]
+6.
+
+class MathUtility {
+    static multiply(a, b) {
+        return a * b;
+    }
 }
 
 // Example usage
-const numbers1 = [10, 20, 5, 30, 25];
-const maxNumber = findMaxNumber(numbers1);
-console.log(`The maximum number is: ${maxNumber}`);
+console.log(MathUtility.multiply(5, 3)); // Output: 15
+console.log(MathUtility.multiply(2, 8)); // Output: 16
+
+
+
+7. class Animal {
+    constructor(name, sound) {
+        this.name = name;
+        this.sound = sound;
+    }
+
+    makeSound() {
+        console.log(`${this.name} says ${this.sound}`);
+    }
+}
+
+class Dog extends Animal {
+    fetch() {
+        console.log("Fetching the ball!");
+    }
+}
+
+// Example usage
+const animal = new Animal("Lion", "Roar");
+animal.makeSound();
+
+const dog = new Dog("Rover", "Woof");
+dog.makeSound();
+dog.fetch();
+
+
+8.
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        console.log(`Hi, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    introduce() {
+        console.log(`Hi, I'm ${this.name}, a ${this.age}-year-old student majoring in ${this.major}.`);
+    }
+}
+
+
+const person1 = new Person("John", 30);
+person1.introduce();
+
+const student1 = new Student("Alice", 20, "Computer Science");
+student1.introduce();
+
+
